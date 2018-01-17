e8ad490fa74a804ec09d841257b45d633f8558c4
chsvk21 <chsvk21@gmail.com>
2018-01-17 12:52:45 -0800

    Ground Work For Scene One
    Completed Ground Work for Scene One
    Change Left and Right Buttons location
    Start Adding Functionality

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// public Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody>().useGravity = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
