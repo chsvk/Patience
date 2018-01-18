68b94654a526760235ab4516d94d22bf4a36a322
chsvk21 <chsvk21@gmail.com>
2018-01-17 23:11:43 -0800

    Implemented Buttons
    Buttons Working but not as expected Need to fix with OnTrigger Events

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// public Rigidbody rigidbody;
+	 public Rigidbody rigidbody;
+	 public Transform PlayerTransform;
+	public Vector3 Start_Position;
+
+	bool Right = false;
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody>().useGravity = true;
+		// rigidbody.useGravity = true;
+		Start_Position.x = 0;
+		Start_Position.y = 1;
+		Start_Position.z = 4.18f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(PlayerTransform.position.y<-1){
+			PlayerTransform.position = Start_Position;
+		}
+
+		if(Right){
+			rigidbody.AddForce(100*Time.deltaTime, 0 ,0, ForceMode.VelocityChange);
+			
+		}
+
+		Right = false;
+	}
+
+	public void Move_Right(){
+		Right = true;
 		
+	}
+
+	public void Move_Left(){
+		rigidbody.AddForce(-100*Time.deltaTime, 0 ,0, ForceMode.VelocityChange);
+	}
+
+	public void Move_Forward(){
+		rigidbody.AddForce(0, 0 , 100* Time.deltaTime, ForceMode.VelocityChange);
+	}
+
+	public void Move_BackWard(){
+		rigidbody.AddForce(0, 0 , -100* Time.deltaTime, ForceMode.VelocityChange);
 	}
 }
