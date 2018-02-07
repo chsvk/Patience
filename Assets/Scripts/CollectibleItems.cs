1bc597bb09046b0ee4cc5a231594a9fa44835481
chsvk21 <chsvk21@gmail.com>
2018-02-07 19:25:20 +0530

    Added Level Complete animation

--- Assets/Scripts/CollectibleItems.cs
+++ Assets/Scripts/CollectibleItems.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class CollectibleItems : MonoBehaviour {
 
 	// Use this for initialization
+	public AudioClip clip;
+	AudioSource source;
 	void Start () {
-		
+		source = GetComponent<AudioSource>();  
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.Rotate(0, 90* Time.deltaTime ,0);
 	}
 
 	private void OnCollisionEnter(Collision other) {
 		if(other.collider.tag == "Player"){
+			source.PlayOneShot(clip, 0.7f);
 			Destroy(gameObject);
 		}
 	}
