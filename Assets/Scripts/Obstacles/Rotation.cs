6ee4eaac03a8af7f223658a82c719e1737ebab0c
chsvk21 <chsvk21@gmail.com>
2018-02-15 11:43:24 +0530

    Created Spikes from Blender

---
+++ Assets/Scripts/Obstacles/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+public GameObject circleGround;
+public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateAround();
+	}
+
+	void RotateAround(){
+		transform.RotateAround(circleGround.transform.position, Vector3.up, speed * Time.deltaTime);
+	}
+}
