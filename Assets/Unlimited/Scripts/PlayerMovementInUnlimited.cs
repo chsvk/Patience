ab0df5bdc8a89e1cbc1ea9249552165e9c6ff5a5
chsvk21 <chsvk21@gmail.com>
2018-03-04 13:06:59 +0530

    Added Unlimited Mode
    Make Some changes for improving playing experience

---
+++ Assets/Unlimited/Scripts/PlayerMovementInUnlimited.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovementInUnlimited : MonoBehaviour {
+	private int speed = 20 ;
+	bool Right;
+	bool Left;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+
+		if(transform.position.y<-1){
+			FindObjectOfType<GameManager>().EndGame();
+		}
+
+		if(Right){
+			Debug.Log("right");
+			GetComponent<Rigidbody>().AddForce(50 * Time.deltaTime,0,0, ForceMode.Impulse);
+		}
+
+		if(Left){
+			GetComponent<Rigidbody>().AddForce(-50 * Time.deltaTime,0,0, ForceMode.Impulse);
+		}
+
+		 GetComponent<Rigidbody>().AddForce(0,0, speed*Time.deltaTime, ForceMode.Impulse);
+	}
+
+	public void onRightDown(bool down){
+		Right = down;
+	}
+
+	public void OnLeftDown(bool down){
+		Left = down;
+	}
+
+}
---
+++ Assets/Unlimited/TileManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+	public GameObject[] tiles;
+	private Transform player;
+	private float SpawnLocationZ = -30.0f;
+	private float tileLength = 36;
+	private float amountOfTilesOnScreen = 5;
+	private float SafeZone = 45;
+	private List<GameObject> activeTiles;
+	private int lastPrefabIndex = 0;
+	private bool StartCompleted = false;
+	// Use this for initialization
+	void Start () {
+		activeTiles = new List<GameObject>();
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		for(int i =0; i<amountOfTilesOnScreen; i++){
+			Debug.Log("Start Method");
+			SpawnTile(-1);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if((player.transform.position.z -SafeZone > (SpawnLocationZ - amountOfTilesOnScreen * tileLength))){
+			// Debug.Log("started");
+			SpawnTile(0);
+			DeleteTile();
+		}
+	}
+
+	void SpawnTile( int prefabIndex){
+		if(prefabIndex == -1){
+			GameObject gor;
+			gor = Instantiate(tiles[0]) as GameObject;
+			gor.transform.SetParent(transform);
+			gor.transform.position = Vector3.forward * SpawnLocationZ;
+			SpawnLocationZ += tileLength;
+			activeTiles.Add(gor);
+		}else{
+		GameObject go;
+		go = Instantiate(tiles[Randome()]) as GameObject;
+		go.transform.SetParent(transform);
+		go.transform.position = Vector3.forward * SpawnLocationZ;
+		SpawnLocationZ += tileLength;
+		activeTiles.Add(go);
+		}
+	}
+
+	void DeleteTile(){
+		Destroy(activeTiles[0]);
+		activeTiles.RemoveAt(0);
+	}
+
+    private int Randome(){
+		if(tiles.Length <=1){
+			return 0;
+		}
+		int randomIndex = lastPrefabIndex;
+		while(randomIndex == lastPrefabIndex){
+			randomIndex = Random.Range(0, tiles.Length);
+		}
+		lastPrefabIndex = randomIndex;
+		return randomIndex;
+	}
+}
